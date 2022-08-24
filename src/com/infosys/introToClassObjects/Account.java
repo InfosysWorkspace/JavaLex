9efccee2321466309034b5ac8e08f26eb2f621b5
Buhari Maiwada <buhari.maiwada@ad.infosys.com>
2022-08-23 22:40:47 -0400

    Completed DataTypes

--- src/com/infosys/introToClassObjects/Account.java
+++ src/com/infosys/introToClassObjects/Account.java
@@ -2,16 +2,27 @@ package com.infosys.introToClassObjects;
 
 public class Account {
 
+    // Instance Variable
     private double balance = 500.00; //state/variable
 
-    public double getBalance(int accountId) { // member method
-        //logic here
-        return balance;
+    // Static Variable
+    static int minimumBalance = 200;
+
+    public double getBalance() { // member method
+
+        // Local Variable
+        int withdrawal = 500;
+
+        return balance - withdrawal;
+    }
+    public void setBalance(int balance){
+        this.balance=balance;
     }
 
     public static void main(String[] args) { // main method
         Account account = new Account(); // Object creation
-        double value = account.getBalance(123456); //method invocation
-        System.out.println("The balance is: " +value);
+        account.setBalance(123456); //method invocation
+        System.out.println(account.balance);
+        System.out.println("The balance is: " +account.getBalance());
     }
 }
