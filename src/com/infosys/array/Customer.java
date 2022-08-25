e9c93043be496a762a181d6427d25ced20cefa9e
Buhari Maiwada <buhari.maiwada@ad.infosys.com>
2022-08-25 15:59:11 -0400

    completed Array

---
+++ src/com/infosys/array/Customer.java
@@ -0,0 +1,15 @@
+package com.infosys.array;
+
+public class Customer {
+    private String name;
+    private String customerId;
+
+    Customer(String uname, String ucustomerId){
+        name = uname;
+        customerId = ucustomerId;
+    }
+
+    public String displayCustomerName(){
+        return name;
+    }
+}
