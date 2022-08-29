7dd616aec9e083ca3fee42cb8b0aab7f74e46528
BsoftMaewada <maewada@live.com>
2022-08-29 16:07:49 -0400

    completed super keyword

---
+++ src/com/infosys/objectOriented/superKeyword/Exercise/OverridingAndSuper.java
@@ -0,0 +1,5 @@
+package com.infosys.objectOriented.superKeyword.Exercise;
+
+public class OverridingAndSuper {
+
+}
---
+++ src/com/infosys/objectOriented/superKeyword/Exercise/SHowYourTalentRegistration.java
@@ -0,0 +1,15 @@
+package com.infosys.objectOriented.superKeyword.Exercise;
+
+public class SHowYourTalentRegistration {
+    public static void main(String[] args) {
+
+        SingleEventRegistration sigle = new SingleEventRegistration("Buhari","Sing&Win", 1);
+        TeanEventRegistration multiple = new TeanEventRegistration("AuraTeam", "ShakeALeg",5,1);
+        SingleEventRegistration single2 = new SingleEventRegistration("Hudson", "PlayAway", 2);
+
+        sigle.registerEvent();
+        multiple.registerEvent();
+        single2.registerEvent();
+
+    }
+}
