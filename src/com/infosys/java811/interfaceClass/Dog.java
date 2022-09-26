e752e0ece7128e66fd302e95c61812feac607490
BsoftMaewada <maewada@live.com>
2022-09-26 18:58:31 -0400

    completed interface and functional interface

---
+++ src/com/infosys/java811/interfaceClass/Dog.java
@@ -0,0 +1,39 @@
+package com.infosys.java811.interfaceClass;
+
+public class Dog {
+    public String cuddle(){
+        return "Purring sounds";
+    }
+
+    public void eat(){
+        System.out.println("Eating stereotypical bones");
+    }
+}
+
+class Cats {
+    public String cuddle(){
+        return "Yawning sound";
+    }
+    public void eat(){
+        System.out.println("Eating stereotypical fish.");
+    }
+}
+
+class PetAdoptionCenter{
+
+    public static void adoptPet(Dog pet){
+        System.out.println("You have successfully adopted the dog.");
+    }
+
+    public static void adoptPet(Cats pet){
+        System.out.println("You have successfully adopted the cat.");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        adoptPet(dog);
+
+        Cats cat = new Cats();
+        adoptPet(cat);
+    }
+}
---
+++ src/com/infosys/java811/interfaceClass/Mammal.java
@@ -0,0 +1,29 @@
+package com.infosys.java811.interfaceClass;
+
+public interface Mammal {
+    void run();
+}
+
+interface Cat extends Mammal{
+    void eat();
+}
+interface Lion extends Cat{}
+
+class Animal implements Lion{
+    public void run(){
+        System.out.println("Pouncing around");
+    }
+
+    public void eat(){
+        System.out.println("Too full to eat!");
+    }
+    // It can have more own methods
+}
+
+class TesterInt{
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.run();
+    }
+}
